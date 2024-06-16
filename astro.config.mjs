56ec1e98ded169887353cc849e293853be3f9874
Rody Huancas <rodyhuancas.04@gmail.com>
2024-06-16 00:57:47 -0500

    Add: configuration for pwa

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,60 @@
+import { VitePWA } from "vite-plugin-pwa";
 import { defineConfig } from "astro/config";
 
 import vercel from "@astrojs/vercel/serverless";
 
+// Helper imports
+import { manifest, seoConfig } from "./src/utils/seoConfig";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+  site: seoConfig.baseURL,
   adapter: vercel(),
   devToolbar: {
-    enabled: false
-  }
+    enabled: false,
+  },
+  vite: {
+    build: {
+      cssMinify: "lightningcss",
+    },
+    ssr: {
+      noExternal: ["path-to-regexp"],
+    },
+    plugins: [
+      VitePWA({
+        registerType: "autoUpdate",
+        manifest,
+        workbox: {
+          globDirectory: ".vercel/output/static",
+          globPatterns: ["**/*.{html,js,css,woff,woff2,ttf,eot,ico}"],
+          runtimeCaching: [
+            {
+              urlPattern: /\.(?:png|jpg|jpeg|svg|gif|webp|avif)$/,
+              handler: "CacheFirst",
+              options: {
+                cacheName: "images",
+                expiration: {
+                  maxEntries: 100,
+                  maxAgeSeconds: 30 * 24 * 60 * 60,
+                },
+              },
+            },
+            {
+              urlPattern: /\.(?:woff|woff2|ttf|eot|ico)$/,
+              handler: "CacheFirst",
+              options: {
+                cacheName: "fonts",
+                expiration: {
+                  maxEntries: 10,
+                  maxAgeSeconds: 30 * 24 * 60 * 60,
+                },
+              },
+            },
+          ],
+          navigateFallback: null,
+        },
+      }),
+    ],
+  },
 });
