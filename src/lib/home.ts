a1963d6bd5fbc5683e69705280be37989f201c8b
Rody Huancas <rodyhuancas.04@gmail.com>
2024-06-20 22:10:54 -0500

    Update: information for home

---
+++ src/lib/home.ts
@@ -0,0 +1,8 @@
+export const HOME_DATA = {
+  logo: "/icons/favicon.svg",
+  aviable: "Disponible para trabajar",
+  name: "Hola, soy Rody",
+  charge: "Desarrollador Full Stack",
+  description:
+    "+1 año como desarrollador Full Stack, me apasiona la tecnología y creo proyectos de código libre. Transformo ideas en realidad a través del código. ¡Bienvenido a mi portafolio!",
+};
